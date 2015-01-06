6d09552f6e21155c8e05bac554b3ecc400868112
huangjihua <steven-online@hotmail.com>
2015-01-06 18:11:41 +0800

    imooc

--- imooc/app.js
+++ imooc/app.js
@@ -5,7 +5,8 @@ var express=require('express');
 var port = process.env.PORT||3000;
 var app  =express();
 
-app.set('views','./views');
+
+app.set('views', './views/pages');
 app.set('view engine','jade');
 app.listen(port);
 
@@ -14,7 +15,35 @@ console.log('imooc started on port '+ port);
 //index page 路由配置
 app.get('/',function(req,res){
         res.render('index',{
-            title:'imooc 首页'
+            title: 'imooc 首页',
+            moives: [
+                {
+                    title: "火线反攻",
+                    _id: 1,
+                    poster: "http://r2.ykimg.com/05160000546F0B4467379F61B70C02CE"
+                },
+                {
+                    title: "火线反攻",
+                    _id: 2,
+                    poster: "http://r2.ykimg.com/05160000546F0B4467379F61B70C02CE"
+                },
+                {
+                    title: "火线反攻",
+                    _id: 3,
+                    poster: "http://r2.ykimg.com/05160000546F0B4467379F61B70C02CE"
+                },
+                {
+                    title: "火线反攻",
+                    _id: 4,
+                    poster: "http://r2.ykimg.com/05160000546F0B4467379F61B70C02CE"
+                },
+                {
+                    title: "火线反攻",
+                    _id: 5,
+                    poster: "http://r2.ykimg.com/05160000546F0B4467379F61B70C02CE"
+                }
+
+            ]
         })
 });
 
@@ -38,6 +67,5 @@ app.get('/admin/movie',function(req,res){
         title:'imooc 管理后台'
     })
 });
-
 
 
